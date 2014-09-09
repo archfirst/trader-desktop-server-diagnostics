02fc2fe6be6ffd3684d251c2dca3f8c7f06d8236
nareshbhatia <naresh.archfirst@gmail.com>
2014-09-09 10:07:43 -0400

    Implemented OrderEventService using SocketIO

--- src/app.js
+++ src/app.js
@@ -1,60 +1,85 @@
-/* global _ */
+/* global _, io */
 'use strict';
 
-angular.module('diagnostics', [ 'ngResource' ]);
+angular.module('diagnostics', [ 'ngResource', 'btford.socket-io' ]);
 
 angular.module('diagnostics')
-    .constant('ServerUrl', 'http://localhost:8080');
+    .constant('ServerUrl', 'http://localhost:8080')
+    .constant('EventType', {
+        orderCreated: 'orderCreatedEvent',
+        placementCreated: 'placementCreatedEvent',
+        executionCreated: 'executionCreatedEvent',
+        allOrdersDeleted: 'allOrdersDeletedEvent'
+    });
 
 angular.module('diagnostics')
-    .controller('DiagnosticsController', function ($scope, TraderService) {
-        $scope.getUsers = function () {
-            $scope.users = TraderService.users();
-        };
+    .controller('DiagnosticsController', function($scope, OrderService, OrderEventService, EventType) {
 
-        $scope.getInstruments = function () {
-            $scope.instruments = TraderService.instruments();
-        };
+        $scope.users = OrderService.users();
+        $scope.instruments = OrderService.instruments();
+        $scope.orders = OrderService.orders();
 
-        $scope.createOrders = function () {
-            var orders = [
-                {
-                    side: 'Buy',
-                    symbol: 'AAPL',
-                    quantity: 99.99,
-                    limitPrice: 998.99,
-                    traderId: 'SS'
-                },
-                {
-                    side: 'Sell',
-                    symbol: 'SAPE',
-                    quantity: 1314.99,
-                    limitPrice: 474.23,
-                    traderId: 'SS'
-                }
-            ];
+        // Returns a random integer between min and max
+        // Using Math.round() will give you a non-uniform distribution!
+        // Based on: https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Math/random
+        function getRandomInt (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
-            _.each(orders, function (order) {
-                TraderService.createOrder(null, order);
-            });
+        function findOrder(id) {
+            return _.find($scope.orders, {id: id});
+        }
 
-            $scope.refreshOrders();
-        };
-
-        $scope.refreshOrders = function () {
-            $scope.orders = TraderService.orders();
-        };
-
-        $scope.clearOrders = function () {
-            TraderService.clearOrders(function () {
-                $scope.refreshOrders();
+        $scope.createOrders = function() {
+            var numInstruments = $scope.instruments.length;
+            OrderService.createOrder(null, {
+                side: 'Buy',
+                symbol: $scope.instruments[getRandomInt(0, numInstruments-1)].symbol,
+                quantity: 5000,
+                limitPrice: 100,
+                traderId: 'SS'                
             });
         };
+
+        $scope.refreshOrders = function() {
+            $scope.orders = OrderService.orders();
+        };
+
+        $scope.clearOrders = function() {
+            OrderService.clearOrders();
+        };
+
+        OrderEventService.on(EventType.orderCreated, function(order) {
+            $scope.orders.push(order);
+        });
+
+        OrderEventService.on(EventType.placementCreated, function(placement) {
+            var order = findOrder(placement.orderId);
+
+            if (order) {
+                order.quantityPlaced += placement.quantityPlaced;
+                order.status = placement.status;
+            }
+        });
+
+        OrderEventService.on(EventType.executionCreated, function(execution) {
+            var order = findOrder(execution.orderId);
+
+            if (order) {
+                order.quantityExecuted += execution.quantityExecuted;
+                order.status = execution.status;
+                order.executionPrice = execution.executionPrice;
+            }
+        });
+
+        OrderEventService.on(EventType.allOrdersDeleted, function() {
+            $scope.orders = [];
+        });
     });
 
 
 angular.module('diagnostics')
-    .factory('TraderService', function ($resource, ServerUrl) {
+    .factory('OrderService', function($resource, ServerUrl) {
 
         return $resource(ServerUrl + '/rest/:type', null, {
             users: {
@@ -85,4 +110,17 @@ angular.module('diagnostics')
             }
         });
 
+    });
+
+
+angular.module('diagnostics')
+    .service('OrderEventService', function(ServerUrl, socketFactory) {
+
+        var socket = socketFactory({
+            ioSocket: io.connect(ServerUrl)
+        });
+
+        this.on = function(eventType, callback) {
+            socket.on(eventType, callback);
+        };
     });
